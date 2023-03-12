712ec9098d6fedf81c0d4cadd1eac92abb31f517
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-03-12 21:01:59 +0100

    Add section component

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import css from '../Section/Section.module.css'
+
+export class Section extends Component {
+    render() {
+        const { title, children } = this.props;
+        return (
+            <>
+                <h2 className={css.title}>{title}</h2>
+                <div className={css.sectionWrapper}>{children}</div>
+            </>
+        )
+    }
+}
