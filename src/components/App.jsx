70b6af44e64146c1a3baeba7c7c2df272daac7d2
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-03-16 19:49:53 +0100

    Refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import { Form } from './Form/Form';
-import { nanoid } from 'nanoid';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 import { ContactList } from './ContactList/ContactList';
@@ -15,15 +14,7 @@ export class App extends Component {
       { id: 'id-3', name: 'Ron Weasley', number: '645-17-79' },
       { id: 'id-4', name: 'Luna Lovegood', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
     filter: '',
-  };
-
-  handleChange = (key, value) => {
-    this.setState({
-      [key]: value,
-    });
   };
 
   filter = e => {
@@ -39,38 +30,10 @@ export class App extends Component {
     );
   };
 
-  reset = () => {
+  addUserToContacts = user => {
     this.setState({
-      name: '',
-      number: '',
+      contacts: [...this.state.contacts, user],
     });
-  };
-
-  submitForm = e => {
-    e.preventDefault();
-    const user = {
-      name: this.state.name,
-      number: this.state.number,
-      id: nanoid(),
-    };
-
-    let contactExists = false;
-
-    this.state.contacts.forEach(contact => {
-      if (contact.name.toLowerCase() === user.name.toLowerCase()) {
-        Notify.info(`${contact.name} is already in the Phonebook.`);
-        contactExists = true;
-      }
-    });
-
-    if (!contactExists) {
-      this.setState({
-        contacts: [...this.state.contacts, user],
-      });
-      Notify.success(`${user.name} was added to the Phonebook.`);
-    }
-
-    this.reset();
   };
 
   deleteContact = identification => {
@@ -92,10 +55,11 @@ export class App extends Component {
       <div className="App">
         <Section title="Phonebook">
           <Form
-            submitForm={this.submitForm}
-            name={this.state.name}
-            number={this.state.number}
-            handleChange={this.handleChange}
+            addUserToContacts={this.addUserToContacts}
+            contacts={this.state.contacts}
+            // name={this.state.name}
+            // number={this.state.number}
+            // handleChange={this.handleChange}
           ></Form>
         </Section>
         <Section title="Contacts">
